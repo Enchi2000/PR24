413b11a63ef88f6e3893415d7854fc17fc428e36
Enchi <juan.sanchezp@udem.edu>
2024-02-15 12:46:47 -0600

    Creates 21684 training images by resizing mnist numbers and uploads prediction of the latest training

--- yolov8.py
+++ yolov8.py
@@ -1,6 +1,6 @@
 from ultralytics import YOLO
 
-model=YOLO('/home/enchi/Documentos/PEF/runs/detect/small/weights/best.pt')
+model=YOLO('/home/enchi/Documentos/PR24/runs/detect/S501/weights/best.pt')
 
-model.predict('/home/enchi/Documentos/PEF/test_images/IC00627P01E20PD00011_2211300001-1.png',save=True)
+model.predict('/home/enchi/Documentos/PR24/test_images/IC00627P01E20PD00011_2211300001-1.png',save=True)
 

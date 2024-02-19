77dfecf1438bb7fe76f8cbcdfabf4be6de346389
Enchi <juan.sanchezp@udem.edu>
2024-02-19 12:55:53 -0600

    Se entrenaron 7 modelos mas

--- yolov8.py
+++ yolov8.py
@@ -1,6 +1,6 @@
 from ultralytics import YOLO
 
-model=YOLO('/home/enchi/Documentos/PR24/runs/detect/S501/weights/best.pt')
+model=YOLO('/home/enchi/Documentos/PR24/runs/detect/M1005/weights/best.pt')
 
 model.predict('/home/enchi/Documentos/PR24/test_images/IC00627P01E20PD00011_2211300001-1.png',save=True)
 

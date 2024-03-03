c6d082124a534b9f9f69e05ac356ec0db5f92816
Enchi <juan.sanchezp@udem.edu>
2024-03-02 20:42:59 -0600

    see results using yolov8

--- results_yolo.py
+++ results_yolo.py
@@ -1,77 +1,30 @@
+from ultralytics import YOLO
 import cv2
 import numpy as np
-import argparse
+import torch
 
-parser=argparse.ArgumentParser()
-parser.add_argument('-i', '--image', required=True,help = 'path to input image')
-parser.add_argument('-c', '--config', required=True,help = 'path to yolo config file')
-parser.add_argument('-w', '--weights', required=True,
-                help = 'path to yolo pre-trained weights')
-parser.add_argument('-cl', '--classes', required=True,
-                help = 'path to text file containing class names')
-args=parser.parse_args()
 
-cv2.namedWindow('Image',cv2.WINDOW_NORMAL)
+# Load a model
+model = YOLO('/home/enchi/Documentos/best.pt')  # pretrained YOLOv8n model
 
-image=cv2.imread(args.image)
-width=image.shape[1]
-height=image.shape[0]
-scale=0.00392
+# classesFile="/home/enchi/Documentos/obj.names"
+# with open(classesFile,'rt') as f:
+#     classes=f.read().rstrip('\n').split('\n')
+#     COLORS=np.random.uniform(0,255,size=(len(classes),3))
 
-net=cv2.dnn.readNet(args.weights,args.config)
+# Run batched inference on a list of images
 
-classes=None
-with open(args.classes,'r') as f:
-    classes=[line.strip() for line in f.readlines()]
-print(len(classes))
+source = cv2.imread('/home/enchi/Documentos/PEF/test_images/IC00627P01E20PD00011_2211300001-1.png')
 
-layer_names = net.getLayerNames()
-indices=net.getUnconnectedOutLayers()
-
-output_layers = [layer_names[int(i) - 1] for i in indices]
-
-# Detecting objects
-blob = cv2.dnn.blobFromImage(image, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
-net.setInput(blob)
-outs = net.forward(output_layers)
-
-# Showing information on the screen
-class_ids = []
-confidences = []
-boxes = []
-for out in outs:
-    for detection in out:
-        scores = detection[5:]
-        class_id = np.argmax(scores)
-        confidence = scores[class_id]
-        if confidence > 0.5:
-            # Object detected
-            center_x = int(detection[0] * width)
-            center_y = int(detection[1] * height)
-            w = int(detection[2] * width)
-            h = int(detection[3] * height)
-            # Rectangle coordinates
-            x = int(center_x - w / 2)
-            y = int(center_y - h / 2)
-            boxes.append([x, y, w, h])
-            confidences.append(float(confidence))
-            class_ids.append(class_id)
-
-indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
-
-font = cv2.FONT_HERSHEY_PLAIN
-colors = np.random.uniform(0, 255, size=(len(classes), 3))
-for i in range(len(boxes)):
-    if i in indexes:
-        x, y, w, h = boxes[i]
-        label = str(classes[class_ids[i]])
-        color = colors[class_id]
-        cv2.rectangle(image, (x, y), (x + w, y + h), color, 1)
-        cv2.putText(image, label, (x-5, y-5), font, 1, color, 1)
-
-cv2.imshow("Image", image)
+results=model(source)
+xyxys=[]
+confidences=[]
+class_ids=[]
+for result in results:
+    boxes=result.boxes.cpu().numpy()
+    xyxys=boxes.xyxy
+    for xyxy in xyxys:
+        cv2.rectangle(source,(int(xyxy[0]),int(xyxy[1])),(int(xyxy[2]),int(xyxy[3])),(0,255,0),1)
+cv2.imshow('test',source)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-
-
-

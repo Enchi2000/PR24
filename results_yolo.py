e411863b7d3af3a47e83964038243cf000d225a1
Enchi <juan.sanchezp@udem.edu>
2024-03-02 21:03:10 -0600

    added class and confidence

--- results_yolo.py
+++ results_yolo.py
@@ -16,15 +16,24 @@ model = YOLO('/home/enchi/Documentos/best.pt')  # pretrained YOLOv8n model
 
 source = cv2.imread('/home/enchi/Documentos/PEF/test_images/IC00627P01E20PD00011_2211300001-1.png')
 
-results=model(source)
-xyxys=[]
-confidences=[]
-class_ids=[]
+results = model(source, conf=0.9, iou=0.5)
+xyxys = []
+confidences = []
+class_ids = []
+
 for result in results:
-    boxes=result.boxes.cpu().numpy()
-    xyxys=boxes.xyxy
-    for xyxy in xyxys:
-        cv2.rectangle(source,(int(xyxy[0]),int(xyxy[1])),(int(xyxy[2]),int(xyxy[3])),(0,255,0),1)
-cv2.imshow('test',source)
+    boxes = result.boxes.cpu().numpy()
+    xyxys.extend(boxes.xyxy)  # Use extend to append multiple values
+    confidences.extend(boxes.conf)
+    class_ids.extend(boxes.cls)
+    for i, xyxy in enumerate(xyxys):
+        x1, y1, x2, y2 = map(int, xyxy)
+        class_name = class_ids[i]  # Assuming class_ids contains class labels
+        confidence = confidences[i]  # Assuming confidences contain confidence scores
+        label = f'{class_name}: {confidence:.2f}'
+        cv2.rectangle(source, (x1, y1), (x2, y2), (0, 255, 0), 1)
+        cv2.putText(source, label, (x1, y1 - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
+
+cv2.imshow('test', source)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
